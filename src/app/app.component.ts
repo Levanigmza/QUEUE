a445a15ab6ed8766b575d7088a83de7f4c375512
l.gamezardashvili <l.gamezardashvili@rs.ge>
2024-07-03 09:55:32 +0400

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,46 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule, HttpHandler } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet  , CommonModule , HttpClientModule],
+  providers:[HttpClient , ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'testapp';
+  currentTranslations: any = {};
+
+  constructor(private http:HttpClient) {
+    this.loadTranslations('geo');
+
+  }
+  Loader: boolean = false;
+
+  changeLanguage(language: string) {
+    this.loadTranslations(language);
+  }
+
+  selectService(service: string) {
+    console.log(`Selected service: ${service}`);
+    this.Loader = true;
+    setTimeout(() => {
+      this.Loader = false;
+    }, 2000);
+  }
+
+  loadTranslations(lang: string) {
+    this.http.get('/assets/translations.json').subscribe(
+      (translations: any) => {
+        this.currentTranslations = translations[lang];
+      },
+      (error) => {
+        console.error(`Error loading translations for ${lang}:`, error);
+      }
+    );
+  }
 }
